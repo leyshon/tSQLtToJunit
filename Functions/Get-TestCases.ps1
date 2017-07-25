9374107ad97f726ca49835d07ba7bdc70aca633e
Michael Leyshon <leyshon@gmail.com>
2017-07-25 16:03:00 +0100

    Trimming the text around the properties we add to fix an issue with spaces

--- Functions/Get-TestCases.ps1
+++ Functions/Get-TestCases.ps1
@@ -19,15 +19,17 @@ Function Get-TestCases {
         $Cases += $Results[$i]
     }
 
+    $Failures = Get-FailureMsg -Results $Results
+    
     $TestCases = @()
     foreach ($Case in $Cases) {
         $Test = New-Object System.object
         $Test | Add-Member -Type NoteProperty -Name id -Value $Case.Split("|")[1]
-        $Test | Add-Member -Type NoteProperty -Name class -Value ($Case.Split("|")[2]).Split(".")[0].TrimEnd()
-        $Test | Add-Member -Type NoteProperty -Name test -Value ($Case.Split("|")[2]).Split(".")[1].TrimEnd()
-        $Test | Add-Member -Type NoteProperty -Name time -Value $Case.Split("|")[3].TrimStart()
-        $Test | Add-Member -Type NoteProperty -Name result -Value $Case.Split("|")[4]
-        $Test | Add-Member -Type NoteProperty -Name reason -Value (Get-FailureMsg -TestName "$($Test.class).$($Test.test)" -Results $Results)
+        $Test | Add-Member -Type NoteProperty -Name class -Value ($Case.Split("|")[2]).Split(".")[0].TrimStart().TrimEnd()
+        $Test | Add-Member -Type NoteProperty -Name test -Value ($Case.Split("|")[2]).Split(".")[1].TrimStart().TrimEnd()
+        $Test | Add-Member -Type NoteProperty -Name time -Value $Case.Split("|")[3].TrimStart().TrimEnd()
+        $Test | Add-Member -Type NoteProperty -Name result -Value $Case.Split("|")[4].TrimStart().TrimEnd()
+        $Test | Add-Member -Type NoteProperty -Name reason -Value ($Failures | Where-Object {$_.test -eq "$($test.class).$($test.test)"}).reason
         $TestCases += $Test
     }
     Return $TestCases
